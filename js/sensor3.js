5f082192e73b5f258da150448b9d4f273c113212
Jadyla <jadylacesariof@gmail.com>
2022-06-28 23:59:17 -0300

    Imagem do SO apresentada de acordo com o SO da máquina

--- js/sensor3.js
+++ js/sensor3.js
@@ -1,4 +1,22 @@
-document.getElementById("hardware").innerHTML = "meu texto";
-
-var img = document.querySelector("#imgSO");
-img.setAttribute('src', '../img/windows.png');
+//Ouvir quando o botão de busca for clicado
+var button = document.querySelector('#busca');
+button.addEventListener('click', function click(e){
+    let ip =  document.getElementById("recebeIp").value;
+    document.querySelector("#printaIp").innerHTML = "IP em consulta: " + "<b>" + ip + "</b>";
+    //alert(data.ip);
+    $.ajax({
+        url: "../sensor3.php",
+        method: "POST",
+        data: { ip: ip},
+        success: function (response) {
+            console.log(response);
+            if (response == "Windows"){
+                var img = document.querySelector("#imgSO");
+                img.setAttribute('src', '../img/windows.png');
+            }else if (response == "Linux"){
+                var img = document.querySelector("#imgSO");
+                img.setAttribute('src', '../img/linux');
+            }
+        }
+    })
+})
---
+++ sensor3.php
@@ -0,0 +1,17 @@
+<?php
+$ip = $_POST["ip"];
+ // print $ip;
+  
+  $community = "public";
+  $oid = "1.3.6.1.2.1.1.1.0";
+
+  $ipInDelivers = snmp2_get($ip,$community ,$oid);
+
+  //retira somente o dado inteiro do retorno da consulta
+  $ipIn = explode("- ", $ipInDelivers);
+  $ipSoft = explode(" ", $ipIn[1]);
+
+  //var_dump($ipIn);
+  //gera a saída que será recebida pelo front-end
+  echo $ipSoft[1];
+?>
